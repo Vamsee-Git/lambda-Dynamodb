e637ec8639827180d984607aa0a854c6aff9f932
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 08:17:26 +0530

    Delete terraform/provider.tf

--- terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "user-data-management-bucket"
-    key            = "terraform"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
-}
